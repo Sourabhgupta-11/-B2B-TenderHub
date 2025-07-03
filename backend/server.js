de90422dcb6b912821e357cc5b4d69293b2506ae
Sourabhgupta-11 <sourabhgupta.1331@gmail.com>
2025-07-04 01:39:17 +0530

    Add cloudinary util to store company logo in cloud

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,6 @@
 const express=require('express');
 const app=express();
-const db=require('./db')
-
-const bodyparser=require('body-parser')
-app.use(bodyparser.json())
+require('./db')
 require('dotenv').config();
 
 const port=process.env.PORT;
