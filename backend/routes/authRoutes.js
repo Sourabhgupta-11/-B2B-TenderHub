c4630b32044fce47d538f520977bfe317a853658
Sourabhgupta-11 <sourabhgupta.1331@gmail.com>
2025-07-04 02:01:46 +0530

    create jwt authentication middleware and use it in authRoutes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const User = require('./../models/User.js');
-
+const {generateToken}=require('./../middleware/auth.js')
 
 router.post('/signup', async (req, res) => {
     try {
         const data = req.body;
         const newUser = new User(data);
         const response = await newUser.save();
-        res.status(200).json({ response });
-    } catch (err) {
-        console.error("Signup error:", err); // 👈 log the actual error
+
+        const payload={
+            id: response.id,
+            email: response.email
+        }
+        const token=generateToken(payload)
+        res.status(200).json({response: response,token: token});
+    } 
+    catch (err) {
+        console.error("Signup error:", err); 
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
@@ -23,8 +30,16 @@ router.post('/login', async (req,res)=>{
         if(!user || !(await user.comparePassword(password))){
             return res.status(401).json({error: 'Invalid email or password'})
         }
+
+        const payload={
+            id: user.id,
+            email: user.email
+        }
+        const token=generateToken(payload)
+        res.json({token})
     }
     catch(err){
+        console.log(err)
         res.status(500).json({err:"Internal Server Error"})
     }
 })
--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,22 @@ const app=express();
 require('./db')
 require('dotenv').config();
 
-const port=process.env.PORT;
+//-------------------------------------------------------------------------//
+app.use(express.json())
 
+
+//-------------------------------TEST ROUTE--------------------------------//
 app.get('/',(req,res)=>{
     res.send("Hello users")
 })
 
-//ROUTES
+//---------------------------------ROUTES----------------------------------//
 const authRoutes=require('./routes/authRoutes.js')
 app.use('/api/auth',authRoutes)
 
+//-------------------------------------------------------------------------//
+
+const port=process.env.PORT;
 app.listen(port,()=>{
     console.log(`App is listening on port ${port}`)
 })
